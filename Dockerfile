2e34ce9cb658e80edb874ce8bfc1a99f47d899ba
Marcus Rejås <marcus.rejas@iotopen.se>
2026-02-27 09:06:01 +0100

    Node-RED 4.1.6

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nodered/node-red:4.1.2
+FROM nodered/node-red:4.1.6
 RUN npm i @iotopen/node-red-contrib-lynx && \
     npm i @node-red-contrib-themes/theme-collection && \
     npm i @iotopen/node-lynx && \
--- build-container.sh
+++ build-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build  -t ghcr.io/iotopen/node-red:4.1.2 --platform linux/arm64,linux/amd64 --push .
+docker buildx build  -t ghcr.io/iotopen/node-red:4.1.6 --platform linux/arm64,linux/amd64 --push .
